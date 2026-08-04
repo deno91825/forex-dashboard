bf492f6ac9a2fa1a0e07ddff7297e9364e74a807
deno91825 <denisogola123@gmail.com>
2026-08-04 23:40:48 +0300

    Create currency search section

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,28 @@
 import { getExchangeRates, getFinancialNews } from "./api.js";
-
 import { displayMessage } from "./ui.js";
-
 import { getFavorites } from "./storage.js";
 
+function setupSearch() {
+    const button = document.querySelector("#search-button");
 
+    button.addEventListener("click", () => {
+        const currency = document.querySelector("#currency-search").value;
+
+        console.log("Searching for:", currency);
+    });
+}
 
 function init() {
-
-
     console.log("Forex Dashboard Started");
 
-
     getExchangeRates();
-
     getFinancialNews();
-
 
     const favorites = getFavorites();
 
+    displayMessage(`Saved favorites: ${favorites.length}`);
 
-    displayMessage(
-        `Saved favorites: ${favorites.length}`
-    );
-
-
+    setupSearch();
 }
-
-
 
 init();
